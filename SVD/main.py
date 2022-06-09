a9d8467b07cd492b969f713add6a94dc4fc82add
pborkowski99 <107076610+pborkowski99@users.noreply.github.com>
2022-06-09 12:58:17 +0200

    Update main.py

--- SVD/main.py
+++ SVD/main.py
@@ -1,34 +1,50 @@
-
 import math
 import numpy as np
 import collections
 import random as rd
 
-def macierz_u(macierz):
-    wiersz1 = macierz[0]
-    wiersz2 = macierz[1]
-    wartosci = np.linalg.eigh(macierz)
+def macierz_u(AAT):
+    wiersz1 = AAT[0]
+    wiersz2 = AAT[1]
+    wartosci = np.linalg.eigh(AAT)
+    wartosci = wartosci[1]
+
+    return wartosci
+
+def macierz_sigma(AAT):
+    wartosci = np.linalg.eigh(AAT)
     wartosci = wartosci[0]
-    lambdy1 = []
-    lambdy1.append([wartosci[0], 0])
-    lambdy1.append([0, wartosci[0]])
-    lambdy2 = []
-    lambdy2.append([wartosci[1], 0])
-    lambdy2.append([0, wartosci[1]])
-    return lambdy1
-    
+    sigma = []
+    sigma.append([wartosci[0], 0, 0])
+    sigma.append([0, wartosci[1], 0])
+    return sigma
+
+def macierz_v(ATA):
+    wartosci = np.linalg.eigh(ATA)
+    wartosci = wartosci[1]
+    return wartosci
 
 
 
 
 
-
-
-
-A = np.array([[1, 2, 0], [2, 1, 0]])
+A = np.array([[1, -2, 0], [0, -2, 1]])
 
 AAT = np.dot(A, A.T)
 ATA = np.dot(A.T, A)
 
-w = macierz_u(AAT)
-print(w)
+U = macierz_u(AAT)
+V = macierz_v(ATA)
+V= V.T
+Sigma = macierz_sigma(AAT)
+print(U)
+print(Sigma)
+print(V)
+'''Wynik z konsoli
+U =[[-0.70710678  0.70710678]
+    [ 0.70710678  0.70710678]]
+Sigma = [[1.0, 0, 0], 
+         [0, 9.0, 0]]
+V = [[-6.66666667e-01 -3.33333333e-01 -6.66666667e-01]
+     [-7.07106781e-01 -1.88247990e-16  7.07106781e-01]
+     [ 2.35702260e-01 -9.42809042e-01  2.35702260e-01]]'''
